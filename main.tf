201b420dbdb09a5f474cb8e8437c0ed9a050e0a7
jcorreaw <juninhocorrea2014@gmail.com>
2023-06-20 19:13:03 -0300

    commit inicial

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "sa-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0af6e9042ea5a4e3e"
+  instance_type = "t2.micro"
+  key_name = "pcjoao"
+  tags = {
+    Name = "Estudo instancia"
+  }
+}
